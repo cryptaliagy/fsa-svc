48869ecef76bfd684fd8e8a83d77b4da89b467be
Natalia Maximo <natalia.maximo@uottawa.ca>
2021-01-28 17:22:50 -0500

    chg: updated the run file

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 if [ "$FLASK_ENV" == "development" ]; then
     gunicorn --bind "$FLASK_RUN_HOST:$FLASK_RUN_PORT" --worker-connections 2 --threads 4 --reload fsa_svc.server:app;
 else
-    gunicorn --bind "$FLASK_RUN_HOST:$FLASK_RUN_PORT" -w 3 --max-requests 1000 --threads 4 fsa_svc.server:app;
+    gunicorn --bind "$FLASK_RUN_HOST:$PORT" -w 3 --max-requests 1000 --threads 4 fsa_svc.server:app;
 fi
